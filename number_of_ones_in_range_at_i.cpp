d7319e12e012faa0f6ce4b269a5d62f6e8452d54
srinivas <srinivaskarri1999@gmail.com>
2020-03-29 13:35:43 +0530

    added comments to number_of_ones_in_range_at_i.cpp file

--- number_of_ones_in_range_at_i.cpp
+++ number_of_ones_in_range_at_i.cpp
@@ -1,4 +1,9 @@
+
+// cnt stores number of 1's in a range from 'l' to 'r-1' (inclusive) at ith bit
+
+// count upto r
 cnt = ((r>>(i+1))<<i);
 if((r>>i)&1) cnt+=(r & ((1ll<<i)-1));
+//sub count upto l from cnt
 cnt -= ((l>>(i+1))<<i);
 if((l>>i)&1) cnt-=(l & ((1ll<<i)-1));
