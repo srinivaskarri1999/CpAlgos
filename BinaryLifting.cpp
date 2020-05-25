c59585f3bc605f77cba29b7a4302fb81dc6ec270
Srinivas Rao <srinivaskarri1999@gmail.com>
2020-05-25 22:49:51 +0530

    updated BinaryLifting.cpp

--- BinaryLifting.cpp
+++ BinaryLifting.cpp
@@ -1,38 +1,31 @@
 //BINARY LIFITING 
 
-int pa[LN][N];
+#define Ln 18 // log2(N)
 
-int dept[N];
+int pa[N][18],d[N];
 
-void pre_dfs(int u,int par) {
-    for(auto ch:G[u]) {
-        if(ch!=par) {
-            pa[0][ch]=u;
-            dept[ch]=dept[u]+1;
-            pre_dfs(ch,u);
+void pre_dfs(int u,int p=-1) {
+    pa[u][0] = p;
+    ifr(i, 1, 18) pa[u][i] = ~pa[u][i-1]?pa[pa[u][i-1]][i-1]:-1;
+    for(int ch:G[u])
+        if(ch != par) {
+            d[ch] = d[u]+1;
+            pre_dfs(ch, u);
         }
-    }
 }
 
 int walk(int u,int k) {
-    ifr(i,0,LN) if((k>>i)&1) u = pa[i][u];
+    ifr(i,0,LN) if((k>>i)&1) u = pa[u][i];
     return u;
 }
 
 int lca(int x,int y) {
-    if(dept[x] < dept[y] ) swap(x,y);
-    x = walk(x,dept[x] - dept[y]);
+    if(d[x] < d[y] ) swap(x,y);
+    x = walk(x, d[x] - d[y]);
     if(x==y) return x;
-    ifr(i,LN,0) if(pa[i][x]!=pa[i][y]) {
-        x = pa[i][x];
-        y = pa[i][y];
+    ifr(i,LN,0) if(pa[x][i]!=pa[y][i]) {
+        x = pa[x][i];
+        y = pa[y][i];
     }
-    return pa[0][x];
-}
-
-void precal(int n){
-    dept[0]=0;
-    pre_dfs(0,0);
-    ifr(i,1,LN) ifr(j,0,n) if(pa[i-1][j]!=-1) 
-        pa[i][j] = pa[i-1][pa[i-1][j]];
+    return pa[x][0];
 }
